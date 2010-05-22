89eb0914b7279ce487be5bf5ff60d53bfc855829
Andrius Bentkus <Andrius.Bentkus@rwth-aachen.de>
2010-05-22 12:46:34 +0200

    Moved files for better directory structure.

---
+++ csharp/src/SteamCondenser/SteamCondenser/OSTypes.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SteamCondenser
+{
+	public enum OSTypes
+	{
+		Unknown,
+		Windows,
+		Linux,
+	}
+}
---
+++ csharp/src/SteamCondenser/SteamCondenser/ServerTypes.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SteamCondenser
+{
+	public enum ServerTypes
+	{
+		Unknown,
+		Listen,
+		Dedicated,
+		Proxy,
+	}
+}
