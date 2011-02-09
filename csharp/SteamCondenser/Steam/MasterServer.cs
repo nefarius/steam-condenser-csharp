fabd2d5fc12dae1c7b03a4694492599660c94fff
Andrius Bentkus <andrius.bentkus@gmail.com>
2011-02-09 23:57:26 +0100

    C#: Alignment fix.
    This just looks nicer.

--- csharp/SteamCondenser/Steam/MasterServer.cs
+++ csharp/SteamCondenser/Steam/MasterServer.cs
@@ -18,15 +18,15 @@ namespace SteamCondenser
 {
 	public enum Regions : byte 
 	{
-		USEastCoast = 0x00,
-		USWestCoast = 0x01,
+		USEastCoast  = 0x00,
+		USWestCoast  = 0x01,
 		SouthAmerica = 0x02,
-		Europe = 0x03,
-		Asia = 0x04,
-		Australia = 0x05,
-		MiddleEast = 0x06,
-		Africa = 0x07,
-		All = 0xFF,
+		Europe       = 0x03,
+		Asia         = 0x04,
+		Australia    = 0x05,
+		MiddleEast   = 0x06,
+		Africa       = 0x07,
+		All          = 0xFF,
 	}
 
 }
