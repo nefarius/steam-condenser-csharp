91a3672eb0cdd55aeaa492b6defc259932a95d7f
Andrius Bentkus <Andrius.Bentkus@rwth-aachen.de>
2011-07-24 04:38:29 +0200

    C#: Tidy up repository.
    We use now Unix new lines, the solution file is set for that.
    We don't need the license in every header, git is enough
    to retrieve all this information.

--- csharp/SteamCondenser.Test/Community/AlienSwarm.cs
+++ csharp/SteamCondenser.Test/Community/AlienSwarm.cs
@@ -1,10 +1,3 @@
-/**
- * This code is free software; you can redistribute it and/or modify it under
- * the terms of the new BSD License.
- *
- * Copyright (c) 2010, Andrius Bentkus
- */
-
 using System;
 using NUnit.Framework;
 using SteamCondenser.Steam.Community;
